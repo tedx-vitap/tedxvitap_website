6bec6c6059f1a86c3cb46d18c033273d379e23c7
Obito <tarunsamala.6435@gmail.com>
2024-04-16 15:28:01 +0530

    Updated Ticket button

--- app/(components)/NavComp.jsx
+++ app/(components)/NavComp.jsx
@@ -8,6 +8,8 @@ import {
   Button,
 } from "@nextui-org/react";
 import { tedxvitapLogo } from "./tedxvitapLogo";
+import  Ticket  from "./svg/ticket_svg";
+
 
 import navLogo from "../../public/navLogo.png";
 import Image from "next/image";
@@ -39,11 +41,16 @@ export default function App() {
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} color="danger" href="#" variant="flat">
-            Purchase Tickets
-          </Button>
+        <Link href="https://vtop1.vitap.ac.in/TEDX/Conferenceinitial" target='_blank' color="foreground">
+                        <div className='flex items-center px-2 py-1 border-2 border-red-900 bg-red-600 hover:bg-red-700 text-red-0 hover:text-red-50 rounded-lg transition duration-150 drop-shadow-[0_2px_2px_rgba(255,46,67,0.5)]'>
+                            <Ticket/>
+                            <span className='pl-1'>Get Tickets</span>
+                        </div>
+                    </Link>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
   );
+
+  
 }
