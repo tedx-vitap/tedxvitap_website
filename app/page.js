a606e75376c353ea6e2a4c9602d9a884feca5185
Obito <tarunsamala.6435@gmail.com>
2024-04-18 22:35:30 +0530

    webp images, bg is low res and blurred with css

--- app/page.js
+++ app/page.js
@@ -4,14 +4,23 @@ import HeroComp from "./(components)/HeroComp";
 import SpeakerSection from "./(components)/SpeakerSection";
 import GalleryComp from "./(components)/GalleryComp";
 import Image from "next/image";
+import Head from "next/head";
+
 export default function Page() {
   return (
     <div>
+
+    <Head>
+        <title lang="en">TEDxVITAP</title>
+        <link rel="icon" href="https://i.postimg.cc/HkV8szgY/fav.webp" />
+        <meta name="TEDxVITAP" content="This is the website for TEDxVITAP"></meta>
+      </Head>
       <section className="fixed w-screen -z-20 flex justify-center">
         <img
-          src="https://i.postimg.cc/tTwdXD7X/Frame-2-2.png"
+          src="https://i.postimg.cc/DyCbTDRp/tedx-bg-min.webp"
           alt="bg-red-image"
-          className="w-screen"
+          className="w-screen blur-lg"
+          id = "tedx-bg"
         />
       </section>
       <HeroComp />
